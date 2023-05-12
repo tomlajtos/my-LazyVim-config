a1be801ef51d9ecad9941e2de712c603e1cd2a46
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-05-12 21:57:12 +0200

    Add telescope-undo and telescope-fzf configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,55 +9,55 @@ return {
       require("telescope").load_extension("undo")
     end,
   },
-  config = {
-    opts = {
-      extensions = {
-        fzf = {
-          fuzzy = true, -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+      undo = {
+        use_delta = true,
+        use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
+        diff_context_lines = vim.o.scrolloff,
+        entry_format = "state #$ID, $STAT, $TIME",
+        side_by_side = false,
+        layout_strategy = "vertical",
+        layout_config = {
+          preview_height = 0.7,
         },
-        undo = {
-          use_delta = true,
-          use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
-          diff_context_lines = vim.o.scrolloff,
-          entry_format = "state #$ID, $STAT, $TIME",
-          side_by_side = false,
-          layout_strategy = "vertical",
-          layout_config = {
-            preview_height = 0.7,
+        keys = {
+          {
+            "<cr>",
+            function()
+              require("telescope-undo.actions").yank_additions()
+            end,
+            mode = "i",
+            { desc = "Telescope-Undo: yank additions" },
           },
-          keys = {
-            {
-              "",
-              function()
-                require("telescope-undo.actions").yank_additions()
-              end,
-              mode = "i",
-              { desc = "test" },
-            },
-            {
-              "",
-              function()
-                require("telescope-undo.actions").yank_deletions()
-              end,
-              mode = "i",
-              { desc = "" },
-            },
-            {
-              "<C-cr>",
-              function()
-                require("telescope-undo.actions").restore()
-              end,
-              mode = "i",
-              { desc = "" },
-            },
+          {
+            "<S-cr>",
+            function()
+              require("telescope-undo.actions").yank_deletions()
+            end,
+            mode = "i",
+            { desc = "Telescope-Undo: yank deletions" },
+          },
+          {
+            "<C-cr>",
+            function()
+              require("telescope-undo.actions").restore()
+            end,
+            mode = "i",
+            { desc = "Telescope-Undo: undo actions" },
           },
         },
       },
     },
   },
+
   keys = {
     {
       "<leader>fp",
