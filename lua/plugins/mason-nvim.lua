c58d51cb4865c58a694a4f22cd167fec4cca5ad0
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-07-25 21:36:21 +0200

    Add prettierd, js-debug-adapter

--- lua/plugins/mason-nvim.lua
+++ lua/plugins/mason-nvim.lua
@@ -1,6 +1,9 @@
 return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    table.insert(opts.ensure_installed, "js-debug-adapter")
     table.insert(opts.ensure_installed, "stylua")
+    table.insert(opts.ensure_installed, "prettierd")
   end,
 }
