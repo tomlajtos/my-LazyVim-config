f08b4297915dbebcfbf7b961bd7836a1b3b4df6c
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-08-04 23:15:20 +0200

    fix: keymap conflict, remove obsolete code

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,22 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- local Util = require("lazyvim.util")
-
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
-
-map("n", "<leader>pv", vim.cmd.Ex, { desc = "Netrw: open explorer" })
-map("i", "jj", "<Esc>", { desc = "<Esc> alternative, insert mode" })
-map("n", "<leader>w", vim.cmd.write, { desc = "Write current buffer" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Netrw: open explorer" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "<Esc> alternative, insert mode" })
+vim.keymap.set("n", "<leader>w", vim.cmd.write, { desc = "Write current buffer" })
 
 -- take line below and append it to the end of the current line with a space
 -- keeps the cursor in the same space as well
@@ -29,17 +16,13 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump 1/2 page down" })
 -- jump to next/prev match while keeping the cursor centered on the screen
 vim.keymap.set("n", "n", "nzzzv", { desc = "" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "" })
--- preserve current paste buffer whan copying over highlighted pattern
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "" })
-
--- deleting to void register  so deleted pattern is not preserved
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "" })
 
 vim.keymap.set("n", "Q", "<nop>", { desc = "" })
+
 -- preserve current paste buffer whan copying over highlighted pattern
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "" })
+-- vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "" })
 
 -- deleting to void register  so deleted pattern is not preserved
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "" })
+-- vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "" })
 
 vim.keymap.set("n", "Q", "<nop>", { desc = "" })
