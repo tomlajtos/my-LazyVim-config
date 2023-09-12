759dde8cea707fce04024bf1556562de1f30c978
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-09-12 16:02:56 +0200

    Add diagnosctics configs to lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -8,6 +8,20 @@ return {
       timeout_ms = 2000,
     },
 
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
+        -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        -- prefix = "icons",
+      },
+      severity_sort = true,
+    },
+
     -- LSP Server Settings
     -- make sure mason installs the server
     ---@type lspconfig.options
