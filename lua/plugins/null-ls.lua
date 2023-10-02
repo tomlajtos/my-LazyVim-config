c2b5758103230dcb1c36208229e6182273e1fe0f
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-10-02 09:08:24 +0200

    refactor(null-ls config): replace null-ls.nvim with none-ls.nvim

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,5 +1,5 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
+  "nvimtools/none-ls.nvim",
   -- event = { "BufReadPre", "BufNewFile" },
   dependencies = { "mason.nvim" },
 
@@ -16,4 +16,17 @@ return {
     table.insert(opts.sources, require("typescript.extensions.null-ls.code-actions"))
     table.insert(opts.sources, nls.builtins.formatting.prettier)
   end,
+  -- opts = function()
+  --   local nls = require("null-ls")
+  --   return {
+  --     root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+  --     sources = {
+  --       nls.builtins.formatting.fish_indent,
+  --       nls.builtins.diagnostics.fish,
+  --       nls.builtins.formatting.stylua,
+  --       nls.builtins.formatting.shfmt,
+  --       -- nls.builtins.diagnostics.flake8,
+  --     },
+  --   }
+  -- end,
 }
