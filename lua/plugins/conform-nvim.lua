ebabd6d108917f77c2dcee909f3607b305531f3e
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-11-28 14:38:44 +0100

    remove custom configs for cmp, lsp and formatting

--- lua/plugins/conform-nvim.lua
+++ lua/plugins/conform-nvim.lua
@@ -1,7 +1,7 @@
 return {
   "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
+  -- dependencies = { "mason.nvim" },
+  -- lazy = true,
   -- cmd = "ConformInfo",
   -- keys = {
   --   {
@@ -22,39 +22,39 @@ return {
   --     end
   --   end)
   -- end,
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      fish = { "fish_indent" },
-      sh = { "shfmt" },
-      ["javascript"] = { { "prettierd", "prettier" } },
-      ["javascriptreact"] = { { "prettierd", "prettier" } },
-      ["typescript"] = { { "prettierd", "prettier" } },
-      ["typescriptreact"] = { { "prettierd", "prettier" } },
-      ["vue"] = { { "prettierd", "prettier" } },
-      ["css"] = { { "prettierd", "prettier" } },
-      ["scss"] = { { "prettierd", "prettier" } },
-      ["less"] = { { "prettierd", "prettier" } },
-      ["html"] = { { "prettierd", "prettier" } },
-      ["json"] = { { "prettierd", "prettier" } },
-      ["jsonc"] = { { "prettierd", "prettier" } },
-      ["yaml"] = { { "prettierd", "prettier" } },
-      ["markdown"] = { { "prettierd", "prettier" } },
-      ["markdown.mdx"] = { { "prettierd", "prettier" } },
-      ["graphql"] = { { "prettierd", "prettier" } },
-      ["handlebars"] = { { "prettierd", "prettier" } },
-    },
+  -- opts = {
+  --   formatters_by_ft = {
+  --     lua = { "stylua" },
+  --     fish = { "fish_indent" },
+  --     sh = { "shfmt" },
+  --     ["javascript"] = { { "prettierd", "prettier" } },
+  --     ["javascriptreact"] = { { "prettierd", "prettier" } },
+  --     ["typescript"] = { { "prettierd", "prettier" } },
+  --     ["typescriptreact"] = { { "prettierd", "prettier" } },
+  --     ["vue"] = { { "prettierd", "prettier" } },
+  --     ["css"] = { { "prettierd", "prettier" } },
+  --     ["scss"] = { { "prettierd", "prettier" } },
+  --     ["less"] = { { "prettierd", "prettier" } },
+  --     ["html"] = { { "prettierd", "prettier" } },
+  --     ["json"] = { { "prettierd", "prettier" } },
+  --     ["jsonc"] = { { "prettierd", "prettier" } },
+  --     ["yaml"] = { { "prettierd", "prettier" } },
+  --     ["markdown"] = { { "prettierd", "prettier" } },
+  --     ["markdown.mdx"] = { { "prettierd", "prettier" } },
+  --     ["graphql"] = { { "prettierd", "prettier" } },
+  --     ["handlebars"] = { { "prettierd", "prettier" } },
+  --   },
     -- LazyVim will merge the options you set here with builtin formatters.
     -- You can also define any custom formatters here.
     ---@type table<string,table>
-    formatters = {
-      injected = { options = { ignore_errors = true } },
+    -- formatters = {
+      -- injected = { options = { ignore_errors = true } },
       -- -- Example of using dprint only when a dprint.json file is present
       -- dprint = {
       --   condition = function(ctx)
       --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
       --   end,
       -- },
-    },
-  },
+    -- },
+  -- },
 }
