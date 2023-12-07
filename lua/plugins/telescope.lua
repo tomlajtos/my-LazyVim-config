746066e50f6f3c6a445a07caee7ba33b786a1773
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-07 22:42:06 +0100

    Remove telescope-fzf config override

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,17 +2,6 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-
-    {
       "debugloop/telescope-undo.nvim",
       config = function()
         require("telescope").load_extension("undo")
