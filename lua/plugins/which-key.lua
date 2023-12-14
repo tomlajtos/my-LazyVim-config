53a1bd72f95534fadce2d899ed266fbfadf4f4dc
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-14 21:49:49 +0100

    Add missing binding names to wich-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    defaults = {
+      ["z"] = { name = "+fold/line" },
+      ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>gw"] = { name = "+git worktree" },
+      ["<leader>p"] = { name = "+project" },
+    },
+  },
+}
