a400fcf6c16d0155fa829b3c39b84e92d0641751
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2023-12-14 21:50:13 +0100

    Add missing binding names for which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,17 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Netrw: open explorer" })
-vim.keymap.set("i", "jj", "<Esc>", { desc = "<Esc> alternative, insert mode" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Netrw explorer (o)" })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Alt escape insert mode" })
 -- take line below and append it to the end of the current line with a space
 -- keeps the cursor in the same space as well
-vim.keymap.set("n", "J", "mzJ`z", { desc = "" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Join curr. line /w line below" })
 -- jump half page up/down (c-d/c-u)
 -- keeps the cursor in the middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump 1/2 page up" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump 1/2 page down" })
 -- jump to next/prev match while keeping the cursor centered on the screen
-vim.keymap.set("n", "n", "nzzzv", { desc = "" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Search: next result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Search: prev result" })
 
-vim.keymap.set("n", "Q", "<nop>", { desc = "" })
+-- disable (no mapping for) capital Q with {rhs} = "<nop>"
+vim.keymap.set("n", "Q", "<nop>", { desc = "none" })
